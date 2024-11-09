b9ee49d1db9de3c3b8196f5bb5ab1969b89d63aa
Gael Girodon <contact@gaelgirodon.fr>
2024-11-10 00:51:44 +0100

    Update dependencies

--- src/util/index.js
+++ src/util/index.js
@@ -4,7 +4,7 @@ import * as glob from '@actions/glob';
  * Returns files matching the glob patterns, sorted by ascending depth
  * and name, excluding some common unwanted directories from the search.
  * @param {string[]} patterns Glob patterns
- * @return {Promise<string[]>} Files sorted by the nearest
+ * @returns {Promise<string[]>} Files sorted by the nearest
  */
 export async function globNearest(patterns) {
   const safePatterns = [
